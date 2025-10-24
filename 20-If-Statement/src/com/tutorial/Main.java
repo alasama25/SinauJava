44c1bfd7bfe5a11a1415bfd8b93ee9bec69ee1c4
alasama25 <alasama351@gmail.com>
2025-10-24 15:14:08 +0700

    p

--- 20-If-Statement/src/com/tutorial/Main.java
+++ 20-If-Statement/src/com/tutorial/Main.java
@@ -1,4 +1,30 @@
 package com.tutorial;
 
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner userInput = new Scanner(System.in);
+
+        int a,b;
+
+        // tutorial untuk if statement atau percabangan
+
+        System.out.print("masukkan nilai a = ");
+        a = userInput.nextInt();
+
+        System.out.print("masukkan nilai b = ");
+        b = userInput.nextInt();
+
+        // ini adalah percabangan
+
+        if ( a > b ){
+            System.out.println("nilai a lebih besar dari nilai b");
+        } else {
+            System.out.println("nilai b lebih besar dari nilai a");
+        }
+
+    }
 }
---
+++ 21-if-else-if-statement/src/com/tutorial/Main.java
@@ -0,0 +1,33 @@
+package com.tutorial;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // tutorial if else if statement
+
+        int a = 5;
+
+        System.out.println("ini adalah awal program");
+
+        // if else if statement
+
+        if ( a == 5){
+
+            System.out.println("ini adalah aksi pertama");
+
+        } else if (a == 10){
+
+            System.out.println("ini adalah aksi kedua");
+
+        } else {
+
+            System.out.println("ini adalah aksi default");
+
+        }
+
+        // ini adalah akhir dari if else if statement
+
+        System.out.println("ini adalah akhir program");
+    }
+}
